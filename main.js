6475f3b755c3bf28f70d35de12f1aa8247a21fe4
yde <philip.ydehed@gmail.com>
2019-11-08 13:44:33 +0100

    hamburgare function media queries

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let startPage = $(".container-startPage");
 let about = $(".container-aboutPage");
 let portfolio = $(".container-portfolioPage");
 let contact = $(".container-contactPage");
+let burger = $(".container-nav")
 
 $(document).ready(function() {
 
@@ -31,6 +32,48 @@ $(document).ready(function() {
         contact.addClass("hidden");
         about.addClass("hidden");
         portfolio.addClass("hidden");
+    });
+
+
+
+
+    $(".burger").click(function() {
+        burger.removeClass("container-nav");
+        
+        $("#about-btn").click(function() {
+            about.removeClass("hidden");
+            startPage.addClass("hidden");
+            contact.addClass("hidden");
+            portfolio.addClass("hidden");
+            burger.addClass("container-nav");
+        });
+    
+        $("#portfolio-btn").click(function() {
+            portfolio.removeClass("hidden");
+            startPage.addClass("hidden");
+            about.addClass("hidden");
+            contact.addClass("hidden");
+            burger.addClass("container-nav");
+
+        });
+    
+        $("#contact-btn").click(function() {
+            contact.removeClass("hidden");
+            startPage.addClass("hidden");
+            about.addClass("hidden");
+            portfolio.addClass("hidden");
+            burger.addClass("container-nav");
+
+        });
+    
+        $("#home-btn").click(function() {
+            startPage.removeClass("hidden");
+            contact.addClass("hidden");
+            about.addClass("hidden");
+            portfolio.addClass("hidden");
+            burger.addClass("container-nav");
+
+        });
     });
 
 });
